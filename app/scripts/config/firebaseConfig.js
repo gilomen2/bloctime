54861ab69189a621681741b45ed1c31ea5b3012a
Beth Gilomen <gilomen2@gmail.com>
2016-08-09 15:34:18 -0500

    pushing config to bb

---
+++ app/scripts/config/firebaseConfig.js
@@ -0,0 +1,7 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB7Ha4dXQijkvFw_fMpOM4_FVjBO-_EL6g",
+  authDomain: "api-project-209863800393.firebaseapp.com",
+  databaseURL: "https://api-project-209863800393.firebaseio.com",
+  storageBucket: "api-project-209863800393.appspot.com",
+};
+firebase.initializeApp(firebaseConfig);
