cb7f871fe0a3af23f4c1ec64349a81e62b448e01
Beth Gilomen <gilomen2@gmail.com>
2016-08-17 18:01:33 -0500

    basic sidebar set up

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -11,6 +11,11 @@
         controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
       });
+    $('.button-collapse').sideNav({
+      menuWidth: 450, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
   }
 
   angular
