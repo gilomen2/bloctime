a16220efd5bdef74d3bc340cc3b32f75b401a576
Beth Gilomen <gilomen2@gmail.com>
2016-08-17 17:29:08 -0500

    play ding at end of a session

--- dist/scripts/services/TimerFactory.js
+++ dist/scripts/services/TimerFactory.js
@@ -4,8 +4,13 @@
 
     function Timer(){
       var timer = this;
-      timer.session = new Session("work", 1500);
+      timer.session = new Session("work", 10);
       timer.completedWorkSessions = 0;
+
+      var mySound = new buzz.sound("assets/audio/ding", {
+        formats: ['mp3'],
+        preload: true
+      });
 
       function Session(type, time){
         var self = this;
@@ -16,7 +21,7 @@
 
         self.start = function(){
           interval = $interval(decrementTime, 1000, self.time);
-          interval.then(adjustCompletedSessions);
+          interval.then(sessionComplete);
           self.state = "running";
         };
 
@@ -28,7 +33,8 @@
           self.time = self.time - 1;
         };
 
-        var adjustCompletedSessions = function(){
+        var sessionComplete = function(){
+          mySound.play();
           if(self.sessionType === "work"){
             timer.completedWorkSessions ++;
           }
@@ -38,13 +44,13 @@
       timer.toggleTimer = function(){
         timer.session.stop();
         if(timer.session.sessionType === "work" && timer.session.state === "stopped" || timer.session.sessionType === "break" && timer.session.state === "running"){
-          timer.session = new Session("work", 1500);
+          timer.session = new Session("work", 10);
         } else if(timer.completedWorkSessions === 4){
           timer.session = new Session("break", 1800);
           timer.completedWorkSessions = 0;
         }
         else {
-          timer.session = new Session("break", 300);
+          timer.session = new Session("break", 3);
         }
         timer.session.start();
       };
@@ -53,9 +59,9 @@
         timer.session.stop();
         timer.session.state = "stopped";
         if(timer.session.sessionType === "work"){
-          timer.session = new Session("work", 1500);
+          timer.session = new Session("work", 10);
         } else if(timer.session.sessionType === "break") {
-          timer.session = new Session("break", 300);
+          timer.session = new Session("break", 3);
         }
       };
     };
