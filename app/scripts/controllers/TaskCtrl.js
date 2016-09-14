96e92315d343206da81135a5d5682eac28a5e222
Beth Gilomen <gilomen2@gmail.com>
2016-09-13 20:30:28 -0500

    working task retrival on sign in, clear from view on sign out

--- app/scripts/controllers/TaskCtrl.js
+++ app/scripts/controllers/TaskCtrl.js
@@ -1,15 +1,32 @@
 (function(){
   function TaskCtrl($scope, UserAuth, UserTasks){
 
-    $scope.user = {};
+    $scope.user = UserAuth.user;
 
-    // $scope.tasks = firebase.database().ref('users/' + UserAuth.user.uid + '/tasks');
-    //
-    // $scope.tasks.on('child_added', function(data) {
-    //   console.log(data.val().task_title);
-    //   displayTask(data.val().task_title);
-    //
-    // });
+    $scope.tasks = null;
+
+    $scope.$on('UserAuth.userAuthenticated', function(){
+      $scope.user = UserAuth.user;
+      $scope.tasks = firebase.database().ref('users/' + $scope.user.uid + '/tasks');
+      createTaskListener();
+    });
+
+    $scope.$on('UserAuth.userSignedOut', function(){
+      $scope.user = null;
+      $scope.tasks = null;
+      clearTasks();
+    });
+
+    var createTaskListener = function(){
+      return $scope.tasks.on('child_added', function(data) {
+        console.log(data.val().task_title);
+        displayTask(data.val().task_title);
+      });
+    };
+
+    var clearTasks = function(){
+      document.querySelector("#user-tasks").innerHTML = '';
+    };
 
     $scope.addTask = function(task){
       UserTasks.createTask($scope.user.uid, task);
@@ -19,14 +36,7 @@
       var el = document.createElement('li');
       el.innerHTML = data;
       document.querySelector("#user-tasks").appendChild(el);
-    }
-
-
-
-    $scope.getTasks = function(){
-
-    }
-
+    };
   };
 
   angular
