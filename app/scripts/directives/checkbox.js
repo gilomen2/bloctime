eff1ef063c49c8c195e910806ceb7f6cd2245901
Beth Gilomen <gilomen2@gmail.com>
2016-09-23 09:17:15 -0500

    working fadout element on markdone. TODO actually remove item from DB and add fadein

--- app/scripts/directives/checkbox.js
+++ app/scripts/directives/checkbox.js
@@ -2,17 +2,17 @@
 
   function checkbox($document){
     return {
-      templateUrl: '/templates/directives/checkbox.html',
+      templateUrl: '../../templates/directives/checkbox.html',
       replace: true,
       restrict: 'E',
       scope: {
-        onChange: '&'
+        task: "=taskData"
       },
       link: function(scope, element, attributes){
         var input = $(element);
 
         scope.markDone = function(task){
-          
+          $(element).fadeOut();
         }
 
 
@@ -25,18 +25,3 @@
     .directive('checkbox', ['$document', checkbox]);
 
 })();
-
-
-var li = document.createElement('li');
-li.id = "li-" + key;
-li.className += "hidden";
-var el = document.createElement('input');
-el.type = "checkbox";
-el.id = key;
-var label = document.createElement('label');
-label.htmlFor = key;
-label.innerHTML = value;
-li.appendChild(el);
-li.appendChild(label);
-document.querySelector("#user-tasks").appendChild(li);
-document.querySelector("#li-" + key).className = "animated fadeIn";
